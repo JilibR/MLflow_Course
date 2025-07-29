000b5ceda70ead39c846c607e45b83078ad43284
Ubuntu <ubuntu@ip-172-31-20-241.eu-west-1.compute.internal>
2025-07-29 13:14:07 +0000

    MLflow Tracking

---
+++ server_mlflow.sh
@@ -0,0 +1,6 @@
+mlflow server \
+  --host 0.0.0.0 \
+  --port 8080 \
+  --backend-store-uri file:///home/ubuntu/MLflow_Course/mlruns \
+  --default-artifact-root file:///home/ubuntu/MLflow_Course/mlruns \
+  --serve-artifacts
